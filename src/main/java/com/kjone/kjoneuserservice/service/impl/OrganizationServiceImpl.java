b8e0783cec9bab326446129b4a0af3b64d04b719
kjone8371 <kjone8371@gmail.com>
2024-09-11 11:35:25 +0900

    조직 auth 서비스 작업중

---
+++ src/main/java/com/kjone/kjoneuserservice/service/impl/OrganizationServiceImpl.java
@@ -0,0 +1,12 @@
+package com.kjone.kjoneuserservice.service.impl;
+
+
+import com.kjone.kjoneuserservice.service.OrganizationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrganizationServiceImpl implements OrganizationService {
+
+}
