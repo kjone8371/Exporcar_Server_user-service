d1862efaf3435719153f5aac5bab8d9442ef3984
kjone8371 <kjone8371@gmail.com>
2024-09-11 11:37:34 +0900

    커스텀 유저 디테일 서비스

---
+++ src/main/java/com/kjone/kjoneuserservice/security/service/impl/CustomUserDetailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.kjone.kjoneuserservice.security.service.impl;
+
+
+import com.kjone.kjoneuserservice.domain.user.User;
+import com.kjone.kjoneuserservice.domain.user.UserForSecurity;
+import com.kjone.kjoneuserservice.repository.UserRepository;
+import com.kjone.kjoneuserservice.security.service.CustomUserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailServiceImpl implements CustomUserDetailService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + email));
+        return UserForSecurity.builder()
+                .user(user)
+                .build();
+    }
+}
+
