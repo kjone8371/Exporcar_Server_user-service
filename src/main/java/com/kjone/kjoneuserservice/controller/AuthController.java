b5a4e5a0db8997539d796d6c845e5fad4434cbb8
kjone8371 <kjone8371@gmail.com>
2024-09-11 11:36:19 +0900

    auth 페이지 컨트롤러

---
+++ src/main/java/com/kjone/kjoneuserservice/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.kjone.kjoneuserservice.controller;
+
+import com.kjone.kjoneuserservice.security.cookie.CookieProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final CookieProvider cookieProvider;
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+}
