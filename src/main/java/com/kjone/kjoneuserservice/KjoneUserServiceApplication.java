c779bbae22f3b4a699c4ab882f7bf87917b4c936
kjone8371 <kjone8371@gmail.com>
2024-09-11 11:39:04 +0900

    유저 서비스

---
+++ src/main/java/com/kjone/kjoneuserservice/KjoneUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kjone.kjoneuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjoneUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KjoneUserServiceApplication.class, args);
+    }
+
+}
