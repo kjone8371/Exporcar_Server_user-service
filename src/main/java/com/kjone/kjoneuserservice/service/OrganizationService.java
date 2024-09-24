6aa1bd5ba664880c189d4190aabe2c3f58433a54
kjone8371 <kjone8371@gmail.com>
2024-09-24 09:21:21 +0900

    조직 서비스

--- src/main/java/com/kjone/kjoneuserservice/service/OrganizationService.java
+++ src/main/java/com/kjone/kjoneuserservice/service/OrganizationService.java
@@ -1,8 +1,10 @@
 package com.kjone.kjoneuserservice.service;
 
 
+import com.kjone.kjoneuserservice.domain.organization.Organization_Request;
+import com.kjone.kjoneuserservice.domain.organization.Organization_Response;
+import com.kjone.kjoneuserservice.domain.organization_user.Organization_User;
 import com.kjone.kjoneuserservice.domain.request.SignRequest;
-import com.kjone.kjoneuserservice.domain.response.SignResponse;
 import com.kjone.kjoneuserservice.domain.user.LoginRequest;
 import com.kjone.kjoneuserservice.domain.user.User;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,12 +12,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 
 public interface OrganizationService {
-    public boolean Organization_signUp(SignRequest signRequest) throws Exception;
-    public SignResponse Organization_signIn(LoginRequest loginRequest) throws Exception;
+    public boolean Organization_signUp(Organization_Request organizationRequest) throws Exception;
+    public Organization_Response Organization_signIn(LoginRequest loginRequest) throws Exception;
     public void logout(HttpServletResponse response);
-    public User findByEmail(String email);
-    public void deleteUserByEmail(String email) throws Exception; // 사용자 삭제 메서드 정의
-    public List<User> getAllUsers();
-    User createProfile(Long userId, SignRequest signRequest) throws Exception;
-    User updateProfile(Long userId, SignRequest signRequest) throws Exception;
+    public Organization_User findByOrgEmail(String email);
+    public void deleteOrgByEmail(String email) throws Exception; // 사용자 삭제 메서드 정의
+    public List<Organization_User> getAllUsers();
+    Organization_User createProfile(Long id, Organization_Request organizationRequest) throws Exception;
+    Organization_User updateProfile(Long id, Organization_Request organizationRequest) throws Exception;
 }
