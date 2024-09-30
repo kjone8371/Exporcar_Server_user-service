0ee5a684e28937d015f13260fbcfac72312bf807
kjone8371 <rlawjddnsj8371@gmail.com>
2024-09-30 13:47:13 +0900

    오류 발견

--- src/main/java/com/kjone/kjoneuserservice/security/service/CustomUserDetailService.java
+++ src/main/java/com/kjone/kjoneuserservice/security/service/CustomUserDetailService.java
@@ -6,6 +6,5 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface CustomUserDetailService extends UserDetailsService {
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException;
 }
